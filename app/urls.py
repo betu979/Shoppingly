6af23ec39e1bc0629b1ae31b960b9a934a37c897
abhilash <abhilash.chaurasiya123@gmail.com>
2024-02-21 00:01:01 +0530

    integrate forget password and profile address

--- app/urls.py
+++ app/urls.py
@@ -3,7 +3,7 @@ from app import views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.auth import views as auth_views
-from .forms import LoginForm,MyPasswordChangeForm
+from .forms import LoginForm,MyPasswordChangeForm,MypasswordResetForm,MysetpasswordForm
 
 urlpatterns = [
     # path('', views.home),
@@ -12,7 +12,7 @@ urlpatterns = [
     path('product-detail/<int:pk>', views.ProductDetailView.as_view(), name='product-detail'),
     path('cart/', views.add_to_cart, name='add-to-cart'),
     path('buy/', views.buy_now, name='buy-now'),
-    path('profile/', views.profile, name='profile'),
+    path('profile/', views.ProfileView.as_view(), name='profile'),
     path('address/', views.address, name='address'),
     path('orders/', views.orders, name='orders'),
     #path('changepassword/', views.change_password, name='changepassword'),
@@ -21,9 +21,14 @@ urlpatterns = [
     path('accounts/logout/', auth_views.LogoutView.as_view(next_page = 'login'), name = 'log-out'),
     #path('login/', views.login, name='login'),
     path('accounts/login/', auth_views.LoginView.as_view(template_name = 'app/login.html', authentication_form = LoginForm), name='login'),
-    path('passwordchange/',auth_views.PasswordChangeView.as_view(template_name = 'app/passwordchange.html', form_class=MyPasswordChangeForm, success_url='/passowordchangedone/'), name='passwordchange'),
+    path('passwordchange/',auth_views.PasswordChangeView.as_view(template_name = 'app/passwordchange.html', form_class=MyPasswordChangeForm, success_url='/passwordchangedone/'), name='passwordchange'),
 
-    path('passwordchangedone/', auth_views.PasswordChangeView.as_view(template_name = 'app/passwordchangedone.html'), name='passowordchangedone'),
+    path('passwordchangedone/', auth_views.PasswordChangeView.as_view(template_name = 'app/passwordchangedone.html'), name='passwordchangedone'),
+    path('password-reset/', auth_views.PasswordResetView.as_view(template_name = 'app/password-reset.html',form_class=MypasswordResetForm), name="password_reset"),
+    path('password-reset/done/', auth_views.PasswordResetDoneView.as_view(template_name = 'app/password-reset-done.html'), name="password_reset_done"),
+    path('password-reset-confirm/<uidb64>/<token>', auth_views.PasswordResetConfirmView.as_view(template_name = 'app/password-reset-confirm.html', form_class=MysetpasswordForm), name="password_reset_confirm"),
+    path('password-reset-complete/', auth_views.PasswordResetCompleteView.as_view(template_name = 'app/password-reset-complete.html'), name="password_reset_complete"),
+
 
     #path('registration/', views.customerregistration, name='customerregistration'),
     path('registration/', views.CustomerRegistrationView.as_view(), name = 'customerregistration'),

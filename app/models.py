7b0437fc7e1e06c098f45b949110f3090fc1a73c
abhilash <abhilash.chaurasiya123@gmail.com>
2024-02-22 23:24:12 +0530

    order placed integrated

--- app/models.py
+++ app/models.py
@@ -78,6 +78,10 @@ class Cart(models.Model):
     
     def __str__(self) -> str:
         return str(self.id)
+
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
     
 STATUS_CHOICES = (
     ('Accepted','Accepted'),
@@ -95,3 +99,6 @@ class OrderPlaced(models.Model):
     ordered_date = models.DateTimeField(auto_now_add = True)
     status = models.CharField(max_length = 50, choices = STATUS_CHOICES, default = "Pending")
     
+    @property
+    def total_cost(self):
+        return self.quantity * self.product.discounted_price
--- app/views.py
+++ app/views.py
@@ -5,6 +5,8 @@ from .forms import CustomerRegistrationForm,CustomerProfileForm
 from django.contrib import messages
 from django.db.models import Q
 from django.http import JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.utils.decorators import method_decorator
 
 #def home(request):
 #  return render(request, 'app/home.html')
@@ -22,11 +24,15 @@ class ProductView(View):
 class ProductDetailView(View):
   def get(self, request, pk):
     product = Product.objects.get(pk=pk)
-    return render(request, 'app/productdetail.html', {'product':product})
+    item_already_in_cart = False
+    if request.user.is_authenticated:
+      item_already_in_cart = Cart.objects.filter(Q(product=product.id) & Q(user=request.user)).exists()
+    return render(request, 'app/productdetail.html', {'product':product,'item_already_in_cart':item_already_in_cart})
 
 #def add_to_cart(request):
  #return render(request, 'app/addtocart.html')
   
+@method_decorator(login_required, name="dispatch")
 class AddToCartView(View):
   def get(self, request):
     user = request.user
@@ -35,6 +41,7 @@ class AddToCartView(View):
     Cart(user=user, product = product).save()
     return redirect('/cart')
 
+@method_decorator(login_required, name="dispatch")
 class ShowCartView(View):
   def get(self, request):
     if request.user.is_authenticated:
@@ -119,13 +126,15 @@ def buy_now(request):
 
 #def profile(request):
 # return render(request, 'app/profile.html')
-
+@login_required
 def address(request):
  add = Customer.objects.filter(user=request.user)
  return render(request, 'app/address.html',{'add':add,'active':'btn-primary'})
 
+@login_required
 def orders(request):
- return render(request, 'app/orders.html')
+ op = OrderPlaced.objects.filter(user = request.user)
+ return render(request, 'app/orders.html',{'order_placed':op})
 
 #def change_password(request):
 # return render(request, 'app/changepassword.html')
@@ -159,9 +168,32 @@ class CustomerRegistrationView(View):
       form.save()
     return render(request, 'app/customerregistration.html', {'form':form}) 
 
+@login_required
 def checkout(request):
- return render(request, 'app/checkout.html')
+ user = request.user
+ add = Customer.objects.filter(user=user)
+ cart_items = Cart.objects.filter(user=user)
+ amount = 0.0
+ shipping_amount = 70.0
+ if cart_items.count() > 0:
+    for p in cart_items:
+      temp_amount = p.quantity * p.product.discounted_price
+      amount += temp_amount
+    totalamount = amount + shipping_amount
+ return render(request, 'app/checkout.html',{'add':add, 'totalamount':totalamount,'cart_items':cart_items})
 
+@login_required
+def payment_done(request):
+  user = request.user
+  custid = request.GET.get('custid')
+  customer = Customer.objects.get(id=custid)
+  cart = Cart.objects.filter(user=user)
+  for c in cart:
+    OrderPlaced(user=user, customer=customer, product=c.product, quantity=c.quantity).save()
+    c.delete()
+  return redirect("orders")
+
+@method_decorator(login_required, name="dispatch")
 class ProfileView(View):
   def get(self, request):
     form = CustomerProfileForm()
